9e374cc0ea8f69feeabd052ac1693d19e1a4d1d7
Daniel Sparber <daniel@sparber.io>
2020-12-14 20:21:54 +0100

    Updated README.md & additional comments

--- cocktails/include/Fluid.h
+++ cocktails/include/Fluid.h
@@ -8,23 +8,27 @@
 class Fluid {
 
 public:
+
+    /// All particles of the fluid
     std::vector<Particle> m_particles;
 
+    /// Human readable name of the fluid
     std::string m_name;
 
+    /* SPH parameters */
+    bool m_isBoundary;
     double m_viscosity;
     double m_stiffness;
     double m_restDensity;
     double m_particleMass;
-    bool m_isBoundary;
     double m_tension;
     double m_tension_thres;
-
     Eigen::Vector3d m_color;
 
     Fluid(std::string name, double viscosity, double stiffness, double restDensity,
           double particleMass, Eigen::Vector3d color, bool isBoundary, double tension = 0.1, double m_tension_thres = 1e-6);
 
+    /// Clear particles
     void reset();
 };
 
--- cocktails/include/FluidSimulation.h
+++ cocktails/include/FluidSimulation.h
@@ -15,41 +15,70 @@ class FluidSimulation : public Simulation {
 public:
     explicit FluidSimulation();
 
+    /// All fluids of the simulation
     std::vector<Fluid*> m_fluids;
+
+    /// All sources of the simulation
     std::vector<Source*> m_sources;
 
+    /// If true, particles are drawn in the color specified by the corresponding fluid
 	bool m_use_particle_color;
 
-    virtual void init() override;
-	virtual void resetMembers() override;
-	virtual bool advance() override;
+	/// Selected surface extractor
+    SurfaceExtractor* m_surface_extractor;
 
-	void updateRenderGeometry() override;
-    void renderRenderGeometry(igl::opengl::glfw::Viewer &viewer) override;
+    /// Output path for the mesh export
+    std::string m_mesh_path;
 
+    /// Output path for the particle export
+    std::string m_particles_path;
+
+    /// Get bounding box of all particles
 	void getMinMaxParticlePosition(Eigen::Vector3d& minPosition, Eigen::Vector3d& maxPosition);
 
+	/// Toggle recording
 	void toggleRecording();
+
+	/// Export particles
 	void exportParticles();
+
+	/// Export mesh
 	void exportMesh();
 
+	/// Set a new scene
 	void setScene(Scene* scene);
 
-	SurfaceExtractor* m_surface_extractor;
 
-	std::string m_mesh_path;
-	std::string m_particles_path;
+    /* Overrides */
+    virtual void init() override;
+    virtual void resetMembers() override;
+    virtual bool advance() override;
+
+    void updateRenderGeometry() override;
+    void renderRenderGeometry(igl::opengl::glfw::Viewer &viewer) override;
 
 protected:
 
+    /// Run the given function in parallel over elementCount elements
 	static void runParallel(int elementCount, const std::function<void(int, int)>& f);
+
+    /// Scene object
 	Scene* m_scene;
 
 private:
-	Eigen::MatrixXd V;  // Vertex positions
-    Eigen::MatrixXi F;  // Faces (indices of vertices)
-	Eigen::MatrixXd C;  // Colors per face
+    /// Vertex positions
+	Eigen::MatrixXd V;
+
+    /// Faces (indices of vertices)
+    Eigen::MatrixXi F;
+
+    /// Colors per face or per vertex
+    Eigen::MatrixXd C;
+
+    /// If true, the simulation state will be saved periodically
 	bool m_save_simulation;
+
+	/// Frequency of simulation state saves
 	int m_save_freq;
 
 };
